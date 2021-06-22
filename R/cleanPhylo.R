320ad45484c782ec78fda96714e87ba77885c75a
andrew-hipp <andrew.hipp@gmail.com>
2021-06-22 14:28:58 -0500

    Data added; cleanPhylo added

---
+++ R/cleanPhylo.R
@@ -0,0 +1,27 @@
+#' Clean a phylogeny for single-tip-per-species analyses
+#'
+#' @param tr A \code{phylo} object
+#' @param delim Character delimiting species names from other info
+#' @param tipElem Integer indicating which element of the tip name contains the species
+#' @param uniquesOnly Boolean: retain only unique tip labels? Generally this is what you want
+#' @param strFixed Boolean: use \code{fixed = TRUE} in \code{strsplit}?
+#' @param gsubFromTo Character, \code{length = 2}; or \code{NA}: if character, use with \code{gsub} to clean tip labels
+#' @param gsubFixed Boolean: use \code{fixed = TRUE} in \code{gsub}?
+cleanPhylo <- function(tr, delim = '|', tipElem = 1,
+                        uniquesOnly = TRUE, strFixed = TRUE,
+                        gsubFromTo = c('_', ' '),
+                        gsubFixed = TRUE) {
+  labsDecomp <- strsplit(tr$tip.label, delim, fixed = strFixed)
+  labsNew <- sapply(labsDecomp, '[', tipElem)
+  if(uniquesOnly) {
+    labsOut <- unique(labsNew)
+    tr <- drop.tip(tr, which(duplicated(labsNew)))
+    tr$tip.label <- labsOut
+  } else {
+    labsOut <- make.unique(labsNew)
+    tr$tip.label <- labsOut
+  }
+  if(!is.na(gsubFromTo[1]))
+    tr$tip.label <- gsub(gsubFromTo[1], gsubFromTo[2], tr$tip.label, fixed = gsubFixed)
+  return(tr)
+}
