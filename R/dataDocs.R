ef9ce368ac803108ccd587dd0d645fc2825d67ff
andrew-hipp <andrew.hipp@gmail.com>
2021-06-23 11:25:20 -0500

    preliminary data documentation

---
+++ R/dataDocs.R
@@ -0,0 +1,28 @@
+#' Morton Arboretum living collections accessions, 2021-06-22
+#'
+#' A dataset containing living collections accessions for _Malus_, _Quercus_,
+#'   _Tilia_ and _Ulmus_ housed at The Morton Arboretum. Data downloaded from
+#'   http://bol.mortonarb.org 2021-06-22. The variables are as follows:
+#'
+#' \itemize{
+#'    \item Taxon.
+#'    \item Vernacular.
+#'    \item Trademark.
+#'    \item ProvenanceType.
+#'    \item DonorInstitute.
+#'    \item CountryName.
+#'    \item PrincipalCollector.
+#'    \item LocationSummary.
+#'    \item BgLocation.
+#'    \item GardenSubarea.
+#'    \item GardenGrid.
+#'    \item GardenSubGrid.
+#'    \item CommemorationText. 
+#'  }
+#' @docType data
+#' @keywords datasets
+#' @name accessions.mor
+#' @usage data(accessions.mor)
+#' @format a `data.frame` of 2173 observations, 14 variables:
+
+NULL
