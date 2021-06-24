3040b0ff1122348af67c3215306ab2d60029a3cd
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2021-06-23 22:22:39 -0500

    ed.calc returns a list of data.frames

--- R/ed.calc.R
+++ R/ed.calc.R
@@ -3,15 +3,19 @@
 #' Calculates evolutionary distinctiveness for trees in a `consInt` object
 #' @param x a `consInt` object
 #' @import phyloregion
-#' @export 
+#' @export
 ed.calc <- function(x) {
   if(class(x) != 'consInt') warning('Expecting a consInt object')
-  out <- data.frame(
-    all.ed.equalSplits = phyloregion::evol_distinct(x$phy.full, 'equal.splits'),
-    all.ed.fairProportion = phyloregion::evol_distinct(x$phy.full, 'fair.proportion'),
-    all.w = w.phy(x$phy.full),
-    sub.ed.equalSplits = phyloregion::evol_distinct(x$phy.sub, 'equal.splits'),
-    sub.ed.fairProportion = phyloregion::evol_distinct(x$phy.sub, 'fair.proportion'),
-    sub.w = w.phy(x$phy.sub)
+  out <- list(
+    all = data.frame(
+      ed.equalSplits = phyloregion::evol_distinct(x$phy.full, 'equal.splits'),
+      ed.fairProportion = phyloregion::evol_distinct(x$phy.full, 'fair.proportion'),
+      w = w.phy(x$phy.full)
+    ),
+    sub = data.frame(
+      ed.equalSplits = phyloregion::evol_distinct(x$phy.sub, 'equal.splits'),
+      ed.fairProportion = phyloregion::evol_distinct(x$phy.sub, 'fair.proportion'),
+      w = w.phy(x$phy.sub)
+    )
   )
 }
