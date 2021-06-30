7f0e8fe3d56eefa67e5ac06c03e6f484944b295a
Andrew Hipp <andrew-hipp@users.noreply.github.com>
2021-06-30 18:43:08 -0500

    minor change in example, ed.calc

---
+++ R/ed.calc.R
@@ -0,0 +1,28 @@
+#' Calculate evolutionary distinctiveness
+
+#' Calculates evolutionary distinctiveness for trees in a `consInt` object
+#' @param x a `consInt` object
+#' @import phyloregion
+#' @examples
+#' example(make.consInt)
+#' temp <- ed.calc(combo.quercus)
+#' plot(all.ed.equalSplits ~ sub.ed.equalSplits, temp$intersect)
+#' @export
+ed.calc <- function(x) {
+  if(class(x) != 'consInt') warning('Expecting a consInt object')
+  intNames <- intersect(x$phy.full$tip.label, x$phy.sub$tip.label)
+  out <- list(
+    all = data.frame(
+      all.ed.equalSplits = phyloregion::evol_distinct(x$phy.full, 'equal.splits'),
+      all.ed.fairProportion = phyloregion::evol_distinct(x$phy.full, 'fair.proportion'),
+      all.w = w.phy(x$phy.full)
+    ),
+    sub = data.frame(
+      sub.ed.equalSplits = phyloregion::evol_distinct(x$phy.sub, 'equal.splits'),
+      sub.ed.fairProportion = phyloregion::evol_distinct(x$phy.sub, 'fair.proportion'),
+      sub.w = w.phy(x$phy.sub)
+    )
+  )
+  out$intersect <- cbind(out$all[intNames, ], out$sub[intNames, ])
+  out
+}
